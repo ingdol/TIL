4542fb4632a4efee2a913451c986cf5bf03ebed1
eunji Shin <65396646+ingdol@users.noreply.github.com>
2022-05-03 23:59:21 +0900

    Create day9.js

---
+++ JavaScript/day9.js
@@ -0,0 +1,17 @@
+// 9개의 수 중 7개의 합이 100이 되는 프로그램
+function solution(arr){
+let answer=arr;
+let sum=answer.reduce((a, b)=>a+b, 0);
+for(let i=0; i<8; i++){
+    for(let j=i+1; j<9; j++){
+        if((sum-(answer[i]+answer[j]))==100){
+            answer.splice(j, 1);
+            answer.splice(i, 1);
+        }
+    }
+}
+return answer;
+}
+
+let arr=[20, 7, 23, 19, 10, 15, 25, 8, 13];
+console.log(solution(arr));
