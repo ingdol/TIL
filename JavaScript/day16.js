fdc54cf7ab6838a792ca7be6d166dd2312f2a2c4
eunji Shin <65396646+ingdol@users.noreply.github.com>
2022-05-11 23:56:55 +0900

    Create day16.js

---
+++ JavaScript/day16.js
@@ -0,0 +1,11 @@
+// 대문자는 소문자로 소문자는 대문자로 변환하여 출력하는 프로그램
+'use strict'
+function solution(s){  
+    let answer="";
+    for (let x of s) {
+        if( x === x.toUpperCase()) answer += x.toLowerCase();
+        else answer += x.toUpperCase();
+    }
+    return answer;
+}
+console.log(solution("StuDY"));
