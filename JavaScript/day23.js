2804551e80e73ac4aae2c384465ee00cc378cdbd
ingdol <dmswl3073@naver.com>
2022-07-15 00:01:59 +0900

    js study - day 23 update

--- JavaScript/day23.js
+++ JavaScript/day23.js
@@ -1,15 +1,18 @@
 // 중복 문자 제거
 function solution(str) {
   let answer = "";
-  let reStr = str;
-  for (let x of str) {
-    let strIndex = reStr.indexOf(x);
-    if (strIndex > -1) {
-      answer += x;
-      let word = new RegExp(x, "g");
-      reStr = reStr.replace(word, "");
-    }
+  for (let x = 0; x < str.length; x++) {
+    if (str.indexOf(str[x]) === x) answer += str[x];
   }
+  //   let reStr = str;
+  //   for (let x of str) {
+  //     let strIndex = reStr.indexOf(x);
+  //     if (strIndex > -1) {
+  //       answer += x;
+  //       let word = new RegExp(x, "g");
+  //       reStr = reStr.replace(word, "");
+  //     }
+  //   }
   return answer;
 }
 let str = "ksekkset";
