9037baa62a3db4822dc012f3e4808dddc5e92540
ingdol <dmswl3073@naver.com>
2022-07-24 23:13:39 +0900

    js study - day29 update

--- JavaScript/day29.js
+++ JavaScript/day29.js
@@ -1,12 +1,32 @@
+// 회문문자열
+// 방법 1
+// function solution(str) {
+//   let answer = "";
+//   let newStr = str.toLowerCase();
+//   let arr = [];
+//   for (let s of newStr) arr.push(s);
+//   for (let s = 0; s < newStr.length; s++) {
+//     if (arr.pop() !== newStr[s]) answer = "NO";
+//     else answer = "YES";
+//   }
+//   return answer;
+// }
+
+// 방법 2
+// function solution(str) {
+//   let answer = "YES";
+//   let n = Math.floor(str.length / 2);
+//   for (let x = 0; x < n; x++) {
+//     if (str[x] !== str[str.length - x - 1]) return "NO";
+//   }
+//   return answer;
+// }
+
+// 방법 3
 function solution(str) {
-  let answer = "";
-  let newStr = str.toLowerCase();
-  let arr = [];
-  for (let s of newStr) arr.push(s);
-  for (let s = 0; s < newStr.length; s++) {
-    if (arr.pop() !== newStr[s]) answer = "NO";
-    else answer = "YES";
-  }
+  let answer = "YES";
+  str = str.toLowerCase();
+  if (str.split("").reverse().join("") !== str) return "NO";
   return answer;
 }
 
