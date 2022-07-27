5b68c7e8f7284e250fc7dfcf972f52330edf0034
ingdol <dmswl3073@naver.com>
2022-07-28 00:31:58 +0900

    js study - day 32 update

--- JavaScript/day32.js
+++ JavaScript/day32.js
@@ -3,45 +3,23 @@ function solution(str, s) {
   for (let x = 0; x < str.length; x++) {
     if (str[x] === s) {
       arr[x] = 0;
-      console.log(x);
       let zero = 0;
       for (let l = x - 1; l >= 0; l--) {
         if (arr[l] !== 0) arr[l] = ++zero;
         else break;
       }
-      //   for (let r = x + 1; r <= str.length; r++) {
-      //     if (arr[r] > ++zero) arr[r] = ++zero;
-      //     else break;
-      //   }
     }
-    console.log(arr);
   }
-  let reArr = arr;
-  for (let x = 0; x < arr.length; x++) {
-    console.log("여기", x, arr[x] === 0);
-    if (arr[x] === 0) {
-      let zero = 0;
-      for (let r = x + 1; r < arr.length; r++) {
-        console.log("출력중", arr[r]);
-        if (arr[r] !== zero) {
-          arr[r] = ++zero;
-          console.log(zero);
-        } else break;
-        // else break;
+  for (let x = 0; x < str.length; x++) {
+    if (str[x] === s) {
+      let zero = 1;
+      for (let l = x + 1; l <= str.length; l++) {
+        if (arr[l] !== 0 && zero <= arr[l + 1]) arr[l] = zero++;
+        else break;
       }
-      console.log(arr);
     }
-    // if (arr[x] === 0) {
-    //   let zero = 0;
-    //   for (let r = x + 1; r <= arr.length; l++) {
-    //     console.log(r);
-    //     if (arr[r] > ++zero) arr[r] = ++zero;
-    //     else break;
-    //   }
-    //   console.log(arr);
-    // }
   }
-  return answer;
+  return arr;
 }
 let str = "teachermode";
 let s = "e";
