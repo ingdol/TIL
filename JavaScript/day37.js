881bf4ebdd1b0983f9752cdbf04d97ddc9f98819
ingdol <dmswl3073@naver.com>
2022-08-03 23:59:03 +0900

    js study - day 37 update

--- JavaScript/day37.js
+++ JavaScript/day37.js
@@ -1,33 +1,51 @@
+// function solution(n, arr) {
+//   let hap = 0;
+//   let answer = 0;
+//   for (let x = 0; x < arr.length; x++) {
+//     hap = arr[x][0] / 2 + arr[x][1];
+//     console.log("반값", hap);
+//     for (let i = 0; i < arr.length; i++) {
+//       if (x !== i) {
+//         console.log(arr[i][0], arr[i][1]);
+//         let arrHap = arr[i][0] + arr[i][1];
+//         hap += arrHap;
+//       }
+//     }
+//     let cnt = arr.length;
+//     for (let i = 0; i < arr.length; i++) {
+//       if (x !== i) {
+//         let arrHap = arr[i][0] + arr[i][1];
+//         if (hap > n) {
+//           hap -= arrHap;
+//           cnt--;
+//           console.log("2번", hap, cnt);
+//         }
+//         if (hap === n) {
+//           answer = cnt;
+//           console.log("최종", hap, cnt);
+//         }
+//       }
+//     }
+//   }
+//   console.log(n, arr);
+//   return answer;
+// }
+
 function solution(n, arr) {
-  let hap = 0;
   let answer = 0;
-  for (let x = 0; x < arr.length; x++) {
-    hap = arr[x][0] / 2 + arr[x][1];
-    console.log("반값", hap);
-    for (let i = 0; i < arr.length; i++) {
-      if (x !== i) {
-        console.log(arr[i][0], arr[i][1]);
-        let arrHap = arr[i][0] + arr[i][1];
-        hap += arrHap;
+  arr.sort((a, b) => a[0] + a[1] - (b[0] + b[1]));
+  for (let i = 0; i < arr.length; i++) {
+    let money = m - (arr[i][0] / 2 + arr[i][0]);
+    let cnt = 1;
+    for (let j = 0; j < arr.length; j++) {
+      if (j !== i && arr[j][0] + arr[j][1] > money) break;
+      if (j !== i && arr[j][0] + arr[j][1] <= money) {
+        money -= arr[j][0] + arr[j][1];
+        cnt++;
       }
     }
-    let cnt = arr.length;
-    for (let i = 0; i < arr.length; i++) {
-      if (x !== i) {
-        let arrHap = arr[i][0] + arr[i][1];
-        if (hap > n) {
-          hap -= arrHap;
-          cnt--;
-          console.log("2번", hap, cnt);
-        }
-        if (hap === n) {
-          answer = cnt;
-          console.log("최종", hap, cnt);
-        }
-      }
-    }
+    answer = Math.max(answer, cnt);
   }
-  console.log(n, arr);
   return answer;
 }
 
