bb1dd5aacd2928620b4c6ef6b6041c142f115525
ingdol <dmswl3073@naver.com>
2022-08-07 23:57:48 +0900

    js study - day 38

--- JavaScript/day38.js
+++ JavaScript/day38.js
@@ -1,11 +1,21 @@
-function solution(arr) {
+function solution(arr, n) {
+  let hapArr = [];
   for (let i = 0; i < arr.length; i++) {
-    let hap = arr[i];
-    for (let j = i; j < arr.length; i++) {
-      hap = arr[j] + arr[j + 1];
+    for (let j = 0; j < arr.length - 1; j++) {
+      if (arr[i] !== arr[j] && arr[i] !== arr[j + 1]) {
+        console.log(arr[i], arr[j], arr[j + 1]);
+        let hap = arr[i];
+        hap = hap + arr[j] + arr[j + 1];
+
+        if (hapArr.indexOf(hap) === -1) hapArr.push(hap);
+      }
     }
-    console.log(hap);
   }
+  console.log(hapArr);
+  hapArr.sort((a, b) => {
+    return b - a;
+  });
+  return hapArr[n - 1];
 }
 let arr = [13, 15, 34, 23, 45, 65, 33, 11, 26, 42];
-console.log(solution(arr));
+console.log(solution(arr, 3));
