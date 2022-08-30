f06bcc99b2dd94874b56570f006ff087e26db086
ingdol <dmswl3073@naver.com>
2022-08-30 23:26:58 +0900

    js study - 연속 부분수열

--- JavaScript/day42.js
+++ JavaScript/day42.js
@@ -1,17 +1,33 @@
-function solution(arr, n) {
-  console.log(arr, n);
-  let lt = (rt = hap = 0);
-  let answer = 0;
-  while (lt < arr.length) {
-    console.log(hap, rt, arr[rt]);
-    hap += arr[rt++];
-    if (hap <= n) {
-      answer++;
-    } else if (rt > arr.length || hap > n) {
-      hap = 0;
-      rt = ++lt;
+// 방법 1
+// function solution(arr, n) {
+//   console.log(arr, n);
+//   let lt = (rt = hap = 0);
+//   let answer = 0;
+//   while (lt < arr.length) {
+//     console.log(hap, rt, arr[rt]);
+//     hap += arr[rt++];
+//     if (hap <= n) {
+//       answer++;
+//     } else if (rt > arr.length || hap > n) {
+//       hap = 0;
+//       rt = ++lt;
+//     }
+//     console.log("answer", answer);
+//   }
+//   return answer;
+// }
+
+// 방법 2
+function solution(arr, m) {
+  let answer = 0,
+    sum = 0,
+    lt = 0;
+  for (let rt = 0; rt < arr.length; rt++) {
+    sum += arr[rt];
+    while (sum > m) {
+      sum -= arr[lt++];
     }
-    console.log("answer", answer);
+    answer += rt - lt + 1;
   }
   return answer;
 }
