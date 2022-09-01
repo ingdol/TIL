2cfc1a27190f83f509eef3601fb4c790f2eb2f91
ingdol <dmswl3073@naver.com>
2022-09-01 23:59:42 +0900

    js study - day43 update

--- JavaScript/day43.js
+++ JavaScript/day43.js
@@ -1,15 +1,29 @@
-function solution(arr, n) {
-  let answer = (hap = lt = rt = 0);
-  while (lt <= arr.length - n) {
-    if (rt < n) {
-      hap += arr[lt + rt];
-      rt++;
-    } else {
-      if (hap > answer) answer = hap;
-      lt++;
-      rt = 0;
-      hap = 0;
-    }
+// 방법 1
+// function solution(arr, n) {
+//   let answer = (hap = lt = rt = 0);
+//   while (lt <= arr.length - n) {
+//     if (rt < n) {
+//       hap += arr[lt + rt];
+//       rt++;
+//     } else {
+//       if (hap > answer) answer = hap;
+//       lt++;
+//       rt = 0;
+//       hap = 0;
+//     }
+//   }
+//   return answer;
+// }
+
+// 방법 2
+function solution(arr, m) {
+  let answer,
+    sum = 0;
+  for (let i = 0; i < m; i++) sum += arr[i];
+  answer = sum;
+  for (let i = m; i < arr.length; i++) {
+    sum += arr[i] - arr[i - m];
+    answer = Math.max(answer, sum);
   }
   return answer;
 }
